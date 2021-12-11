048bda5b86533f5ce6a569602cba5e3266f47931
kyung <tmdrud41@gmail.com>
2021-12-11 19:28:04 +0900

    theme changed

---
+++ _posts/2021-12-11-TestPosting.md
@@ -0,0 +1,13 @@
+2021-12-11-TestPosting.md
+--- 
+title: "블로그 시작" 
+excerpt: "첫 포스팅 " 
+last_modified_at: 
+categories:
+ - Blog 
+tags:
+ - content
+ - Blog 
+ ---
+
+# 포스팅 테스트
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Testposting",
+        "excerpt":"2021-12-11-TestPosting.md  title: “블로그 시작”  excerpt: “첫 포스팅 “  last_modified_at:  categories:     Blog  tags:   content   Blog   —   포스팅 테스트  ","categories": [],
+        "tags": [],
+        "url": "/TestPosting/",
+        "teaser": null
+      }]
